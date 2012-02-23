37c4ca9de4ed10bfa0d523b3bb781614806b72ec
koppen <koppen@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2012-02-23 12:56:05 +0000

    added patches directory with first Torbrowser patch

--- JonDoBrowser/trunk/build/fetch_FF_source.sh
+++ JonDoBrowser/trunk/build/fetch_FF_source.sh
@@ -1,40 +1,43 @@
-#!/bin/sh
+#!/bin/bash
 
-# This script fetches the sources of a Firefox release and verifies them.
+# This script fetches the latest sources of a Firefox release and verifies them.
+version=$(wget -qO - http://releases.mozilla.org/pub/mozilla.org/firefox/releases/latest/source | \
+  grep -Eom 1 '[0-9]+\.[0-9]\.[0-9]' | tail -n1)
 
-# TODO: We do not want to hardcode the version.
-VERSION=9.0.1
-
-if [ ! -e "firefox-${VERSION}.source.tar.bz2" ]; then
-  echo "Getting the Firefox sources for version ${VERSION}...\n"
-  wget http://releases.mozilla.org/pub/mozilla.org/firefox/releases/${VERSION}/source/firefox-${VERSION}.source.tar.bz2
+#TODO: Mitigation of downgrade attacks
+if [ "$version" = "" ]; then
+  echo "We got no version extracted, thus exiting..."
+  exit 1
+elif [ ! -e "firefox-$version.source.tar.bz2" ]; then
+  echo "Getting the latest Firefox sources ..."
+  wget http://releases.mozilla.org/pub/mozilla.org/firefox/releases/latest/source/firefox-$version.source.tar.bz2
   if [ ! $? -eq 0 ]; then
-    echo "Error while retrieving the Firefox source, exiting...\n"
+    echo "Error while retrieving the Firefox sources, exiting..."
     exit 1
   fi
 fi
 
-if [ ! -e "firefox-${VERSION}.source.tar.bz2.asc" ]; then
-  echo "Getting the signature...\n"
-  wget http://releases.mozilla.org/pub/mozilla.org/firefox/releases/${VERSION}/source/firefox-${VERSION}.source.tar.bz2.asc
+if [ ! -e "firefox-$version.source.tar.bz2.asc" ]; then
+  echo "Getting the signature..."
+  wget http://releases.mozilla.org/pub/mozilla.org/firefox/releases/latest/source/firefox-$version.source.tar.bz2.asc
   if [ ! $? -eq 0 ]; then
-    echo "Error while retrieving the signature, exiting...\n"
+    echo "Error while retrieving the signature, exiting..."
     exit 1
   fi
 fi
 
-echo "Checking the signature...\n"
+echo "Checking the signature..."
 # TODO: Implement a more generic routine her assuming the user has not yet
 # imported the Firefox key
 # gpg prints the verification success message to stderr
-SIGKEY=`gpg --verify firefox-${VERSION}.source.tar.bz2.asc 2>&1 | \
-  grep -Eom 1 '([A-Z0-9]{4}\s*){10}' | tr -d ' '`
+sigkey=$(gpg --verify firefox-$version.source.tar.bz2.asc 2>&1 | \
+  grep -Eom 1 '([A-Z0-9]{4}\s*){10}' | tr -d ' ')
 # TODO: Is it enough to check just the main key??
-if [ "${SIGKEY}" = "9D03193D6BDC541BD796C4E47F4D66451EBCAB3A" ]; then
+if [ "$sigkey" = "9D03193D6BDC541BD796C4E47F4D66451EBCAB3A" ]; then
   echo "Successful verification!"
 fi
 
-mv firefox-${VERSION}.source.tar.bz2 ../../build_env && cd ../../build_env
-tar -xjvf firefox-${VERSION}.source.tar.bz2
+mv firefox-$version.source.tar.bz2 ../../build_env && cd ../../build_env
+tar -xjvf firefox-$version.source.tar.bz2
 
-echo "Ready for building JonDoBrowser..."
+echo "Patching JonDoBrowser..."
