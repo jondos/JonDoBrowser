9e68dfa9e6407234dec26010320d42494dc87b44
koppen <koppen@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2012-03-02 09:27:02 +0000

    more generic name for language patches dir; bugfixing fetch script

--- build/fetch_patch_ff_source.sh
+++ build/fetch_patch_ff_source.sh
@@ -5,8 +5,10 @@
 # The first grep makes sure we really get the latest firefox version and not
 # someting else of the html page. The second grep finally extracts the latest
 # version.
-version=$(wget -qO - http://releases.mozilla.org/pub/mozilla.org/firefox/releases/latest/source | \
-  grep -Eom 1 'firefox-[0-9]{2}\.[0-9](\.[0-9]).source.tar.bz2' | tail -n1 |\
+
+releasePath="http://releases.mozilla.org/pub/mozilla.org/firefox/releases/latest/source/"
+version=$(wget -qO - $releasePath | \
+  grep -Eom 1 'firefox-[0-9]{2}\.[0-9](\.[0-9]).source.tar.bz2' | tail -n1 | \
    grep -Eom 1 '[0-9]{2}\.[0-9](\.[0-9])')
 
 #TODO: Mitigation of downgrade attacks
@@ -15,7 +17,7 @@ if [ "$version" = "" ]; then
   exit 1
 elif [ ! -e "firefox-$version.source.tar.bz2" ]; then
   echo "Getting the latest Firefox sources ..."
-  wget http://releases.mozilla.org/pub/mozilla.org/firefox/releases/latest/source/firefox-$version.source.tar.bz2
+  wget ${releasePath}firefox-$version.source.tar.bz2
   if [ ! $? -eq 0 ]; then
     echo "Error while retrieving the Firefox sources, exiting..."
     exit 1
@@ -24,7 +26,7 @@ fi
 
 if [ ! -e "firefox-$version.source.tar.bz2.asc" ]; then
   echo "Getting the signature..."
-  wget http://releases.mozilla.org/pub/mozilla.org/firefox/releases/latest/source/firefox-$version.source.tar.bz2.asc
+  wget ${releasePath}firefox-$version.source.tar.bz2.asc
   if [ ! $? -eq 0 ]; then
     echo "Error while retrieving the signature, exiting..."
     exit 1
@@ -40,16 +42,25 @@ sigkey=$(gpg --verify firefox-$version.source.tar.bz2.asc 2>&1 | \
 
 if [ "$sigkey" = "247CA658AA95F6171EB0F13EA7D75CC7C52175E2" ]; then
   echo "Successful verification!"
+else
+  echo "Wrong signature, aborting..."
+  exit 1
 fi
 
-mv firefox-$version.source.tar.bz2 ../../build_env && cd ../../build_env
+if [ ! -d "build" ]; then
+  mkdir build 
+fi
+
+cd build && cp ../firefox-$version.source.tar.bz2 .
 tar -xjvf firefox-$version.source.tar.bz2
 
 echo
 echo "Patching JonDoBrowser..."
 
-svn co https://svn.jondos.de/svnpub/JonDoBrowser/trunk/build/patches \
-  1>/dev/null
+if [ ! -d "patches" ]; then
+  svn export https://svn.jondos.de/svnpub/JonDoBrowser/trunk/build/patches \
+    1>/dev/null
+fi
 cp patches/*.patch mozilla-release/ && cd mozilla-release
 
 # Essentially the patch-any-src.sh from the Torproject
