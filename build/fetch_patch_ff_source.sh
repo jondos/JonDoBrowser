ae03c58a62ae60ccfbb5e0f9a30339a70f6a92c7
koppen <koppen@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2012-03-02 16:22:59 +0000

    updated fetch and patch script; and the en-US prefs file

--- build/fetch_patch_ff_source.sh
+++ build/fetch_patch_ff_source.sh
@@ -6,10 +6,32 @@
 # someting else of the html page. The second grep finally extracts the latest
 # version.
 
-releasePath="http://releases.mozilla.org/pub/mozilla.org/firefox/releases/latest/source/"
-version=$(wget -qO - $releasePath | \
+# We only need the german language pack currently as english is the default
+xpiLang=de
+platforms="linux-i686 mac win32"
+mozKey=247CA658AA95F6171EB0F13EA7D75CC7C52175E2 
+releasePath=http://releases.mozilla.org/pub/mozilla.org/firefox/releases/latest/
+version=$(wget -t 3 -qO - ${releasePath}source | \
   grep -Eom 1 'firefox-[0-9]{2}\.[0-9](\.[0-9]).source.tar.bz2' | tail -n1 | \
    grep -Eom 1 '[0-9]{2}\.[0-9](\.[0-9])')
+
+gpgVerification() {
+  file=$1
+  sigKey=$(gpg --verify $1 2>&1 | \
+    grep -Eom 2 '([A-Z0-9]{4}\s*){10}' | tail -n1 | tr -d ' ')
+
+  if [ "$sigKey" = "$mozKey" ]; then
+    echo "Successful verification!"
+  else
+    echo "Wrong signature, aborting..."
+    exit 1
+  fi 
+}
+
+if [ ! -d "tmp" ]; then
+  mkdir tmp
+fi
+cd tmp
 
 #TODO: Mitigation of downgrade attacks
 if [ "$version" = "" ]; then
@@ -17,7 +39,7 @@ if [ "$version" = "" ]; then
   exit 1
 elif [ ! -e "firefox-$version.source.tar.bz2" ]; then
   echo "Getting the latest Firefox sources ..."
-  wget ${releasePath}firefox-$version.source.tar.bz2
+  wget -t 3 ${releasePath}source/firefox-$version.source.tar.bz2
   if [ ! $? -eq 0 ]; then
     echo "Error while retrieving the Firefox sources, exiting..."
     exit 1
@@ -26,32 +48,64 @@ fi
 
 if [ ! -e "firefox-$version.source.tar.bz2.asc" ]; then
   echo "Getting the signature..."
-  wget ${releasePath}firefox-$version.source.tar.bz2.asc
+  wget -t 3 ${releasePath}source/firefox-$version.source.tar.bz2.asc
   if [ ! $? -eq 0 ]; then
     echo "Error while retrieving the signature, exiting..."
     exit 1
   fi
 fi
 
-echo "Checking the signature..."
+echo "Checking the signature of the sources..."
 # TODO: Implement a more generic routine her assuming the user has not yet
 # imported the Firefox key
 # gpg prints the verification success message to stderr
-sigkey=$(gpg --verify firefox-$version.source.tar.bz2.asc 2>&1 | \
-  grep -Eom 1 '([A-Z0-9]{4}\s*){10}' | tr -d ' ')
+gpgVerification firefox-$version.source.tar.bz2.asc
 
-if [ "$sigkey" = "247CA658AA95F6171EB0F13EA7D75CC7C52175E2" ]; then
-  echo "Successful verification!"
-else
-  echo "Wrong signature, aborting..."
+echo "Gettings the necessary language packs..."
+echo "Fetching and verifying the SHA1SUMS file..."
+
+wget -t 3 ${releasePath}SHA1SUMS
+if [ ! $? -eq 0 ]; then
+  echo "Error while retrieving SHA1SUMS, exiting..."
   exit 1
 fi
+
+wget -t 3 ${releasePath}SHA1SUMS.asc
+if [ ! $? -eq 0 ]; then
+  echo "Error while retrieving the SHA1SUMS signature, exiting..."
+  exit 1
+fi
+
+gpgVerification SHA1SUMS.asc
+
+echo "Retrieving the language pack(s) and verifying them..."
+
+for platform in $platforms; do
+  wget -t 3 -O ${platform}_${xpiLang}.xpi ${releasePath}${platform}/xpi/$xpiLang.xpi 
+  if [ ! $? -eq 0 ]; then
+    echo "Error while retrieving the $xpiLang language pack for"
+    echo "${platform}, continuing without it..."
+    #continue
+  fi 
+  xpiHash1=$(grep -E "\./$platform/xpi/$xpiLang.xpi" SHA1SUMS | \
+    grep -Eo "[a-z0-9]{40}")
+  xpiHash2=$(sha1sum ${platform}_${xpiLang}.xpi | grep -Eo "[a-z0-9]{40}") 
+  echo "$platformHash"
+  if [ "$xpiHash1" = "$xpiHash2" ]; then
+    echo "Verified SHA1 hash..."
+  else
+    echo "Wrong SHA1 hash of ${platform}_${xpiLang}.xpi" 
+    rm ${platform}_${xpiLang}.xpi
+  fi
+done
+
+cd ..
 
 if [ ! -d "build" ]; then
   mkdir build 
 fi
 
-cd build && cp ../firefox-$version.source.tar.bz2 .
+cd build && cp ../tmp/firefox-$version.source.tar.bz2 .
 tar -xjvf firefox-$version.source.tar.bz2
 
 echo
@@ -63,7 +117,7 @@ if [ ! -d "patches" ]; then
 fi
 cp patches/*.patch mozilla-release/ && cd mozilla-release
 
-# Essentially the patch-any-src.sh from the Torproject
+# Essentially the patch-any-src.sh from the Tor Project
 for i in *patch; do patch -tp1 <$i || exit 1; done
 
 exit 0
