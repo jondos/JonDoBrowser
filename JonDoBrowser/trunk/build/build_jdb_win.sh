030127290605e8321f0d52f475ba6382c1755229
koppen <koppen@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2012-05-23 10:15:23 +0000

    no -OO flag for pymake anymore

--- JonDoBrowser/trunk/build/build_jdb_win.sh
+++ JonDoBrowser/trunk/build/build_jdb_win.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd jdbBuild/mozilla-release
-python -OO build/pymake/make.py -f client.mk
+python build/pymake/make.py -f client.mk
 cd win32_build
-python -OO ../build/pymake/make.py package
+python ../build/pymake/make.py package
 cd dist
 mv firefox*.zip ../../../
 cd ../../
