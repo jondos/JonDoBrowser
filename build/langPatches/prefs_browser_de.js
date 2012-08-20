960c38cb18ef5f477f28519a9b0beb464cac4abf
koppen <koppen@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2012-08-20 10:44:17 +0000

    fixed bug in JDB prefs files (now only the add-ons from the active profile are used/accessed

--- build/langPatches/prefs_browser_de.js
+++ build/langPatches/prefs_browser_de.js
@@ -77,6 +77,7 @@ user_pref("extensions.adblockplus.showinstatusbar", false);
 user_pref("extensions.adblockplus.showintoolbar", true);
 user_pref("extensions.adblockplus.showsubscriptions", false);
 user_pref("extensions.adblockplus.savestats", false);
+user_pref("extensions.enabledScopes", 1);
 user_pref("extensions.jondofox.last_version", "");
 user_pref("extensions.jondofox.new_profile", false);
 user_pref("extensions.jondofox.browser_version", "0.0.1");
