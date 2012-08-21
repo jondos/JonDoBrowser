421ed66205789068c17b9346d6a6f49ef0f0d284
koppen <koppen@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2012-08-21 12:16:07 +0000

    updated win build script

--- JonDoBrowser/trunk/build/build_jdb_win.sh
+++ JonDoBrowser/trunk/build/build_jdb_win.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 
-cd jdbBuild/mozilla-release
+svnBrowser=https://svn.jondos.de/svnpub/JonDoBrowser/trunk
+
+# Now, extracting, patching, rebranding and building JonDoBrowser...
+if [ ! -d "build" ]; then
+  mkdir build
+fi
+
+# Assuming we got the verified FF source copied to tmp first...
+cd build && cp ../tmp/firefox-*.source.tar.bz2 .
+tar -xjvf firefox-*.source.tar.bz2
+echo
+echo "Patching JonDoBrowser..."
+
+if [ ! -d "patches" ]; then
+  svn export $svnBrowser/trunk/build/patches 1>/dev/null
+fi
+
+cp patches/*.patch mozilla-release/ && cd mozilla-release
+
+svn export $svnBrowser/build/branding/jondobrowser browser/branding/jondobrowser
+
+# Essentially the patch-any-src.sh from the Tor Project
+for i in *patch; do patch -tp1 <$i || exit 1; done
+
+echo "Building JonDoBrowser..."
+svn cat $svnBrowser/build/.mozconfig_win > .mozconfig
+
 python build/pymake/make.py -f client.mk
 cd win32_build
 python ../build/pymake/make.py package
