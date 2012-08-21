51c07e7baf3d3812241b912727e96e55cf8cf40a
koppen <koppen@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2012-08-21 14:44:16 +0000

    updated Mac build script

--- build/build_jdb_mac.sh
+++ build/build_jdb_mac.sh
@@ -24,8 +24,8 @@ generateDmgImage() {
   # http://stackoverflow.com/questions/96882/how-do-i-create-a-nice-looking-dmg-for-mac-os-x-using-command-line-tools
   # Adapted for JonDoBrowser's needs by Georg Koppen, JonDos GmbH 2012.
 
-  applicationName="JonDoBrowser-mac-$1.app"
-  finalDMGName="JonDoBrowser-$1.dmg"
+  applicationName="JonDoBrowser-mac-$jdbVersion-$1.app"
+  finalDMGName="JonDoBrowser-mac-$jdbVersion-$1.dmg"
   # TODO GeKo: Why does the backslash as a line delimiter not work but result in an error?
   # UDBZ?
   hdiutil create -srcfolder "${source}" -volname "${title}" -fs HFS+ -fsargs "-c c=64,a=16,e=16" -format UDRW -size ${size}k JDB.temp.dmg
@@ -46,8 +46,8 @@ generateDmgImage() {
            set icon size of theViewOptions to 128
            set background picture of theViewOptions to file ".background:'${backgroundPictureName}'"
            make new alias file at container window to POSIX file "/Applications" with properties {name:"Applications"}
-           set position of item "'${applicationName}'" of container window to {120, 130}
-           set position of item "Applications" of container window to {380, 130} 
+           set position of item "'${applicationName}'" of container window to {100, 120}
+           set position of item "Applications" of container window to {375, 120} 
            update without registering applications
            delay 5
            eject
@@ -91,14 +91,14 @@ prepareMacProfiles() {
   local appDir
   local dataDir
   local profileDir
-  local jdbPlatform="JonDoBrowser-mac"
 
   for lang in $langs; do
-    appDir=$jdbPlatform-$lang/Contents/MacOS
+    jdbDir=JonDoBrowser-mac-$jdbVersion-$lang
+    appDir=$jdbDir/Contents/MacOS
     dataDir=$appDir/Firefox.app/Contents/MacOS/Data
-    profileDir=$jdbPlatform-$lang/Library/Application\ Support/Firefox/Profiles/profile
+    profileDir=$jdbDir/Library/Application\ Support/Firefox/Profiles/profile
     mkdir -p $appDir/Firefox.app/Contents/Resources
-    mkdir $jdbPlatform-$lang/Contents/Resources
+    mkdir $jdbDir/Contents/Resources
     mkdir -p $dataDir/profile
     mkdir $dataDir/plugins
     mkdir -p "$profileDir"
@@ -107,8 +107,8 @@ prepareMacProfiles() {
     mv -f "$profileDir"/places.sqlite_$lang "$profileDir"/places.sqlite
     rm -f "$profileDir"/places.sqlite_*
     cp JonDoBrowser $appDir
-    cp Info.plist $jdbPlatform-$lang/Contents
-    cp jondobrowser.icns $jdbPlatform-$lang/Contents/Resources
+    cp Info.plist $jdbDir/Contents
+    cp jondobrowser.icns $jdbDir/Contents/Resources
     # Copying the language xpi to get other language strings than the en-US
     # ones.
     # TODO: We need to get that copied from the Linux machine first...
@@ -119,7 +119,7 @@ prepareMacProfiles() {
 }
 
 cleanup() {
-  #Cleanup: Imitating make...
+  # Cleanup: Imitating make...
   echo "Cleaning up everything..."
   rm -rf tmp && rm -rf build
   exit 0
@@ -160,8 +160,8 @@ if [ ! -d "build" ]; then
 fi
 
 # Assuming we got the verified FF source copied to tmp first...
-cd build && cp ../tmp/firefox-$ffVersion.source.tar.bz2 .
-tar -xjvf firefox-$ffVersion.source.tar.bz2
+cd build && cp ../tmp/firefox-*.source.tar.bz2 .
+tar -xjvf firefox-*.source.tar.bz2
 echo
 echo "Patching JonDoBrowser..."
 
@@ -178,15 +178,16 @@ svn export $svnBrowser/build/branding/jondobrowser browser/branding/jondobrowser
 for i in *patch; do patch -tp1 <$i || exit 1; done
 
 echo "Building JonDoBrowser..."
-svn cat $svnBrowser/build/.mozconfig_mac > build/mozilla-release/.mozconfig
+svn cat $svnBrowser/build/.mozconfig_mac > .mozconfig
 make -f client.mk build
 
 echo "Creating the final packages..."
 cd ../..
 for lang in $langs; do
-  appDir=JonDoBrowser-mac-$lang/Contents/MacOS
-  cp -rf build/mozilla-release/mac_build/dist/Firefox.app/* $appDir/Firefox.app 
-  mv JonDoBrowser-mac-$lang JonDoBrowser-mac-$lang.app
+  jdbDir=JonDoBrowser-mac-$jdbVersion-$lang
+  cp -rf tmp/$jdbDir .
+  cp -rf build/mozilla-release/mac_build/dist/JonDoBrowser.app/* $jdbDir/Contents/MacOS/Firefox.app
+  mv $jdbDir $jdbDir.app
   # Preparing everything for generating the dmg image...
   if [ ! -d $source ]; then
     mkdir $source
@@ -194,7 +195,7 @@ for lang in $langs; do
     svn cat $svnBrowser/build/mac/background-$lang.png > ${backgroundPictureName}
     cd ../..
   fi
-  mv JonDoBrowser-mac-$lang.app $source/
+  mv $jdbDir.app $source/
   generateDmgImage $lang
   rm -rf $source
 done
