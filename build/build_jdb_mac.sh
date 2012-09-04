a5902e37f1bb6f2093b007b19c5e42123c5a152d
koppen <koppen@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2012-09-04 14:07:24 +0000

    updated Mac build script

--- build/build_jdb_mac.sh
+++ build/build_jdb_mac.sh
@@ -41,7 +41,7 @@ generateDmgImage() {
            set current view of container window to icon view
            set toolbar visible of container window to false
            set statusbar visible of container window to false
-           set the bounds of container window to {400, 100, 885, 430}
+           set the bounds of container window to {400, 100, 885, 450}
            set theViewOptions to the icon view options of container window
            set arrangement of theViewOptions to not arranged
            set icon size of theViewOptions to 128
