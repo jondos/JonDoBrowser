402755582986a6307332e08cbcb817ae0bd96332
koppen <koppen@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2012-10-04 09:10:09 +0000

    automated win build procedure

--- JonDoBrowser/trunk/build/build_jdb_win.sh
+++ JonDoBrowser/trunk/build/build_jdb_win.sh
@@ -1,8 +1,129 @@
 #!/bin/bash
 
+# Copyright (C) 2012 Jondos GmbH
+
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#  * Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright notice, 
+#    this list of conditions and the following disclaimer in the documentation 
+#    and/or other materials provided with the distribution.
+#  * Neither the name of the JonDos GmbH nor the names of its contributors may 
+#    be used to endorse or promote products derived from this software without 
+#    specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON 
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# This script fetches the latest sources of a Firefox release and verifies them.
+# Afterwards the browser profiles are prepared and JonDoBrowser for Windows is
+# built.
+
 svnBrowser=https://svn.jondos.de/svnpub/JonDoBrowser/trunk
 FFPath=profile/Firefox/App/firefox
 FFEnPath=profile/FirefoxByLanguage/enFirefoxPortablePatch/App/firefox
+mozKey=247CA658AA95F6171EB0F13EA7D75CC7C52175E2
+releasePath=http://releases.mozilla.org/pub/mozilla.org/firefox/releases/latest
+GnuPGPATH="/c/Programm Files (x86)/GNU/GnuPG/pub/"
+
+cleanup() {
+  #Cleanup: Imitating make...
+  echo "Cleaning up everything..."
+  rm -rf tmp && rm -rf build
+  exit 0
+}
+
+gpgVerification() {
+  sigKey=$(gpg --verify $1 2>&1 | \
+    grep -Eom 2 '([A-Z0-9]{4}\s*){10}' | tail -n1 | tr -d ' ')
+
+  if [ "$sigKey" = "$mozKey" ]; then
+    echo "Successful verification!"
+  else
+    echo "Wrong signature, aborting..."
+    exit 1
+  fi 
+}
+
+# Setting the path to the GnuPG executable
+export PATH=$PATH:${GnuPGPath}
+
+OPTSTR="ch"
+getopts "${OPTSTR}" CMD_OPT
+while [ $? -eq 0 ];
+do
+  case ${CMD_OPT} in
+    c) cleanup;;
+    h) echo '' 
+       echo 'JonDoBrowser Build Script 1.0 (2012 Copyright (c) JonDos GmbH)'
+       echo "usage: $0 [options]"
+       echo ''
+       echo 'Possible options are:'
+       echo '-c removes old build cruft.'
+       echo '-h prints this help text.'
+       echo ''
+       exit 0
+       ;;
+  esac
+  getopts "${OPTSTR}" CMD_OPT
+done
+
+# The first grep makes sure we really get the latest firefox version and not
+# someting else of the html page. The second grep finally extracts the latest
+# version.
+# When we are using 'wget' in this script we retry three times if necessary
+# as some mirrors of releases.mozilla.org seem to be not reachable at times...
+echo "Getting the latest Firefox source version..."
+ffVersion=$(wget -t 3 -qO - $releasePath/source | \
+  grep -Eom 1 'firefox-[0-9]{2}\.[0-9](\.[0-9])*.source.tar.bz2' | tail -n1 | \
+   grep -Eom 1 '[0-9]{2}\.[0-9](\.[0-9])*')
+
+if [ ! -d "tmp" ]; then
+  mkdir tmp
+fi
+cd tmp
+
+#TODO: Mitigation of downgrade attacks
+if [ "$ffVersion" = "" ]; then
+  echo "We got no version extracted, thus exiting..."
+  exit 1
+elif [ ! -e "firefox-$ffVersion.source.tar.bz2" ]; then
+  echo "Getting the latest Firefox sources ..."
+  wget -t 3 $releasePath/source/firefox-$ffVersion.source.tar.bz2
+  if [ ! $? -eq 0 ]; then
+    echo "Error while retrieving the Firefox sources, exiting..."
+    exit 1
+  fi
+fi
+
+if [ ! -e "firefox-$ffVersion.source.tar.bz2.asc" ]; then
+  echo "Getting the signature..."
+  wget -t 3 $releasePath/source/firefox-$ffVersion.source.tar.bz2.asc
+  if [ ! $? -eq 0 ]; then
+    echo "Error while retrieving the signature, exiting..."
+    exit 1
+  fi
+fi
+
+echo "Checking the signature of the sources..."
+# TODO: Implement a more generic routine her assuming the user has not yet
+# imported the Firefox key
+# gpg prints the verification success message to stderr
+gpgVerification firefox-$ffVersion.source.tar.bz2.asc
+
+cd ..
 
 # Now, extracting, patching, rebranding and building JonDoBrowser...
 if [ ! -d "build" ]; then
@@ -10,7 +131,7 @@ if [ ! -d "build" ]; then
 fi
 
 # Assuming we got the verified FF source copied to tmp first...
-cd build && cp ../tmp/firefox-*.source.tar.bz2 .
+cd build && cp ../tmp/firefox-$ffVersion.source.tar.bz2 .
 tar -xjvf firefox-*.source.tar.bz2
 echo
 echo "Patching JonDoBrowser..."
@@ -33,7 +154,7 @@ python build/pymake/make.py -f client.mk
 cd win32_build
 python ../build/pymake/make.py package
 cd dist
-mv firefox*.zip ../../../
+mv firefox-$ffVersion.en-US.win32.zip ../../../
 cd ../../
 rm -rf win32_build
 cd ../../
@@ -41,7 +162,7 @@ cd ../../
 # Now preparing the installer
 echo "Now we gonna build the installer..."
 svn export $svnBrowser/build/win profile
-unzip -d profile/Firefox/App build/firefox*.zip
+unzip -d profile/Firefox/App build/firefox-$ffVersion.en-US.win32.zip
 
 # Moving language specific files...
 mkdir -p $FFEnPath
