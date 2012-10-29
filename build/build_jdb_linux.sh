c70c9b80520926c10430b652c60099408094e701
koppen <koppen@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2012-10-29 07:13:14 +0000

    prepared 0.2.2 bugfix release

--- build/build_jdb_linux.sh
+++ build/build_jdb_linux.sh
@@ -39,7 +39,7 @@ xpiLang=de
 # Allowing 32bit and 64bit JonDoBrowser builds
 platform="linux-$(uname -m)"
 jdbDir="JonDoBrowser"
-jdbVersion="0.2"
+jdbVersion="0.2.2"
 # TODO: Shouldn't we check whether this one is still used/valid before actually
 # building? Maybe that's something which is related to the more generic routine
 # for the case the key was not imported yet which is mentioned below.
