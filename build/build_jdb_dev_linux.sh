de5cf56eba00c672930e7848f527bd5dc27feee5
koppen <koppen@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2013-05-08 11:09:13 +0000

    code clean-up; changed compression algorithm from bzip2 to xz

--- build/build_jdb_dev_linux.sh
+++ build/build_jdb_dev_linux.sh
@@ -347,8 +347,8 @@ for lang in $langs; do
       awk '{print $5}')/g" update_$updateFinal.xml
   fi
   tar -cf $jdbFinal.tar $jdbDir
-  bzip2 -z9 $jdbFinal.tar
-  mv $jdbFinal.tar.bz2 ../
+  xz -z9 $jdbFinal.tar
+  mv $jdbFinal.tar.xz ../
   rm -rf $jdbDir
   # TODO: Only needed if we need to build another localized build...
   cd ../build/mozilla-release
