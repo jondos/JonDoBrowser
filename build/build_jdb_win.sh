c5fd593d15f0891897d3b4c81b603caf65f90181
koppen <koppen@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2013-05-21 08:41:50 +0000

    removed white space

--- build/build_jdb_win.sh
+++ build/build_jdb_win.sh
@@ -9,11 +9,11 @@
 #
 #  * Redistributions of source code must retain the above copyright notice, this
 #    list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above copyright notice, 
-#    this list of conditions and the following disclaimer in the documentation 
+#  * Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
 #    and/or other materials provided with the distribution.
-#  * Neither the name of the JonDos GmbH nor the names of its contributors may 
-#    be used to endorse or promote products derived from this software without 
+#  * Neither the name of the JonDos GmbH nor the names of its contributors may
+#    be used to endorse or promote products derived from this software without
 #    specific prior written permission.
 
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
