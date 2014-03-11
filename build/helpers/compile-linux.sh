e450cb2cfca66da4a59cbdd1887300cb99a62f30
ness <ness@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2014-03-11 10:25:05 +0000

    Update Debain build scripts

--- build/helpers/compile-linux.sh
+++ build/helpers/compile-linux.sh
@@ -2,18 +2,12 @@
 
 platform="linux-$(uname -m)"
 
-cp -f .mozconfig_$platform > buildtmp/mozilla-release/.mozconfig
-
 cd buildtmp/mozilla-release
 make -f client.mk build
 
 cd linux_build 
-
-# replace search engines
-
-
 make package
 
 cd ../../..
 
-cp buildtmp/mozilla-release/dist/firefox-*.en-US.$platform.tar.bz2 buildtmp/firefox.en-US.tar.bz2
+cp buildtmp/mozilla-release/linux_build/dist/firefox-*.en-US.$platform.tar.bz2 buildtmp/firefox.tar.bz2
