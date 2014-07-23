ff3113b0af98c17e18fd6269a0d84152d4d813ad
ness <ness@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2014-07-23 07:37:11 +0000

    update JDF version to 0.17

--- JonDoBrowser/trunk/build/helpers/fetch-ff-source.sh
+++ JonDoBrowser/trunk/build/helpers/fetch-ff-source.sh
@@ -5,7 +5,7 @@
 # --- initialize / export  basic vars ---
 #
 
-jdbVersion="0.16"
+jdbVersion="0.17"
 
 mozKey=5445390EF5D0C2ECFB8A6201057CC3EB15A0A4BC
 export releasePath=http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/latest-24.0esr
